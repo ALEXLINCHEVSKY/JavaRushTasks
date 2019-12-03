d1ab62e839d9d5007fe233a915f1cff996a5dae8
Alex A Linch <44601498+ALEXLINCHEVSKY@users.noreply.github.com>
2019-12-03 13:46:01 +0300

    Create python_learn1.py

---
+++ python_learn1.py
@@ -0,0 +1,18 @@
+global name, age, status, sex
+
+def mankind ():
+   print("Please. Enter your name")
+   input(name)
+   print("Please. Enter your age")
+   input(age)
+   print("Please. Enter your status")
+   input(status)
+   print("Please. Enter your sex")
+   input(sex)
+   print("Your name is " +name+ ". And you are " +age+ " years old. You are " +status+ ". And you are " +sex+ ".") 
+    
+def main():
+    print("Hello, i'a python app.")
+    mankind()
+    
+main()
